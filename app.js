75b1878403eb1bb107788f0048b839727b64c799
Stephanie Miller <stephanie.miller923@gmail.com>
2021-03-16 09:02:34 -0500

    Completed Spread Destruct Func Exercise

---
+++ app.js
@@ -0,0 +1,56 @@
+const jPDinos = [
+    "Velociraptors",
+    "Tyrannosaurus Rex",
+    "Dilophosaurus"
+];
+
+function seeDinos(a, ...b){
+    console.log(a);
+    console.log(b);
+}
+seeDinos(...jPDinos);
+
+console.log("=========");
+
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+function seeCharacters({ellieSattler}){
+    console.log(ellieSattler);
+}
+seeCharacters(jPCharacters);
+
+console.log("===========");
+
+const jurassicParkMovies = [
+    {
+        one: "Jurassic Park",
+        two: "The Lost World",
+        three: "Jurassic Park III"
+    },
+    {
+        four: "Jurassic World",
+        five: "Jurassic World: Fallen Kingdom"
+    }
+];
+
+function seePJMovies([{one, two, three}, {four, five}]){
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+}
+seePJMovies(jurassicParkMovies);
+// OR
+// function seePJMovies({one, two, three}, {four, five}){
+//     console.log(one);
+//     console.log(two);
+//     console.log(three);
+//     console.log(four);
+//     console.log(five);
+// }
+// seePJMovies(...jurassicParkMovies);
